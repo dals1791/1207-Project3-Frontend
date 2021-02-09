2ea5df4dd8923352189fda16f366f1fd09b9d849
aria414 <ayng13@yahoo.com>
2021-02-09 11:58:35 -0500

    updated pie chart to render fetched data

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ import Transaction from "./components/Transaction";
 import UserLogin from "./components/UserLogin";
 
 function App() {
+  const url = "http://localhost:4000/users";
+
+  // ----------------------- Defines STATES -----------------------
+  const [user, setUser] = React.useState(null);
+
+  // ============= USEEFFECT FUNCTION TO GET DATA =============
+
+  const getUser = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setUser(data);
+  };
+
+  // fetch dogs when page loads
+  React.useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
       <AddIncome />
-      <Landing />
+      <Landing user={user} />
       <Team />
       <Transaction />
       <UserLogin />
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -5,22 +5,78 @@ import ChartDoughnutLabels from "chartjs-plugin-doughnutlabel";
 
 const PieChart = (props) => {
   const [chartData, setChartData] = React.useState({});
+  const { transactions, budget } = props;
 
+  console.log("Chart Transaction Props: ", transactions);
+  console.log("Chart Budget Props: ", budget);
+
+  /* ------------------------------------------------------
+  - FUNCTION TO GET THE NON-REPEATING CATEGORIES FOR TRANSACTIONS
+  ------------------------------------------------------ */
+
+  // Get an array of just the categories from the transactions object -  NO DUPLICATE VALUES
+  // CITATION --- https://www.codegrepper.com/code-examples/javascript/array+map+distinct
+  const categories = transactions
+    .map((transaction) => transaction.category)
+    .filter((value, index, self) => self.indexOf(value) === index);
+
+  console.log("unique catgories: ", categories);
+
+  /* ------------------------------------------------------
+    1. Define an empty array of objects 'categoryTransact' - to hold each category's transactions info
+    2. Loop through the CATEGORIES array. Filter all the items with the specific transaction category. These items are objects.
+    3. For every item in that same category, add up the 'AMOUNT' property
+    4. Push the total amount spend from each category into the array of objects 'categorySum'
+  ------------------------------------------------------ */
+  // ------ STEP 1 -------
+  let categoryTransact = [];
+  let categorySum = [];
+
+  // ------- STEP 2 -------
+  categories.forEach((category) => {
+    let sum = 0;
+    categoryTransact = transactions.filter((transaction) => {
+      return transaction.category === category;
+    });
+    // -- STEP 3 --
+    categoryTransact.forEach((item) => (sum += item.amount));
+    // -- STEP 4 ---
+    categorySum.push({
+      category: category,
+      totalAmout: sum,
+    });
+  });
+
+  console.log("Total spending for each category: ", categorySum);
+  //MAP the totalAmount key in the array of objects into a new array with just the $ totalAmount
+  const spendData = categorySum.map((item) => item.totalAmout);
+
+  // Push the remaining balance amount into the spendData array
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  spendData.push(budget[0].income - spendData.reduce(reducer, 0));
+  // Push "Remaining" as a category into the categories array
+  categories.push("Remaining");
+
+  /* ------------------------------------------------------
+     USE EFFECT TO SET THE CHART DATA 
+    ------------------------------------------------------ */
   React.useEffect(() => {
     setChartData(
       {
-        labels: ["Remaining", "Food", "Bills", "Groceries", "Gas", "Pet Stuff"],
+        // labels: ["Remaining", "Food", "Bills", "Groceries", "Gas", "Pet Stuff"],
+        labels: categories,
         datasets: [
           {
             label: "Spendings",
-            data: [1000, 325, 1240, 456, 350, 290],
+            //data: [1000, 325, 1240, 456, 350, 290],
+            data: spendData,
             backgroundColor: [
-              "rgba(38, 173, 108, 0.8)",
               "rgba(201, 62, 62, 0.8)",
               "rgba(54, 162, 235, 0.8)",
               "rgba(191, 146, 42, 0.8)",
               "rgba(153, 102, 255, 0.8)",
               "rgba(242, 147, 58, 0.8)",
+              "rgba(38, 173, 108, 0.8)",
             ],
             borderWidth: 1,
             borderColor: "#fff",
@@ -81,7 +137,7 @@ const PieChart = (props) => {
                   color: "green",
                 },
                 {
-                  text: "$1000.00",
+                  text: "$" + budget[0].income,
                   font: {
                     size: "30",
                   },
