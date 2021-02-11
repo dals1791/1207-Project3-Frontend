c901844a56ba9217a533f48787cee7664c8710f4
Steven <sastudillo6@gmail.com>
2021-02-11 06:18:32 -0500

    team pages desktop styling complete.

--- src/components/Team.js
+++ src/components/Team.js
@@ -6,15 +6,23 @@ const Team = () => {
       <div className="team-header">
       <h1> Our Team </h1>
       </div>
-
+      <div className="our-team-header">
+        <h2 className="team-header-desktop">Our Team</h2>
+      </div>
+      <div className="flex-team-member-card">
       <div className="team-member-card1">
         <div className="avatar"></div>
         <div className="name-header1">
         <h4>Anny</h4>
         </div>
         <div className="paragraph-team1">
-          <p>Blah blah blah something about this awesome team member.</p>
+          <p>Front-End/Back-End for user pages.</p>
           </div>
+          <div className="paragraph-team1-after">
+          <p>Hobbies: Drawing, loves to hike, be out in nature, unfortunately do not have a car so cannot do so that often. 
+            .</p>
+          </div>
+
       </div>
       <div className="team-member-card2">
       <div className="avatar"></div>
@@ -22,7 +30,10 @@ const Team = () => {
         <h4>Sean</h4>
       </div>
       <div className="paragraph-team2">
-          <p>blah blah blah something about this awesome team member.</p>
+          <p>Back-End set-up, configuration and assisted with front-end pages and routes.</p>
+          </div>
+          <div className="paragraph-team2-after">
+          <p>Sean is a fellow student from the General Assembly Software Engineering Immersive. He has two cats.</p>
           </div>
       </div>
           <div className="team-member-card3">
@@ -32,6 +43,10 @@ const Team = () => {
         </div>
         <div className="paragraph-team3">
           <p>blah blah blah something about this awesome team member.</p>
+          </div>
+          <div className="paragraph-team3-after">
+          <p>blah blah blah something about this awesome team member.</p>
+          </div>
           </div>
       </div>
     </div>
