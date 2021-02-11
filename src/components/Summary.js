fadf044ce6f18ff08b212733974506cb77844b4f
aria414 <ayng13@yahoo.com>
2021-02-10 22:49:32 -0500

    mobile layout for landing and transaction

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,22 +4,20 @@ const Summary = (props) => {
   console.log("Summary props: ", props);
 
   return (
-    <div style={{ border: "1px solid red" }}>
+    <div className="spend-summary">
       <h1> Spend Summary </h1>
-      <section>
-        <h2>${props.budget[0].income}</h2>
-        <h3>Income</h3>
-      </section>
 
-      <section>
-        <h2>${props.totalSpent}</h2>
-        <h3>Total Spent</h3>
-      </section>
+      <div>
+        <section className="spendSum-total">
+          <h2>${props.totalSpent}</h2>
+          <h3>Total Spent</h3>
+        </section>
 
-      <section>
-        <h2>${props.budget[0].income - props.totalSpent}</h2>
-        <h3>Remaining Balance</h3>
-      </section>
+        <section className="spendSum-remain">
+          <h2>${props.budget[0].income - props.totalSpent}</h2>
+          <h3>Remaining</h3>
+        </section>
+      </div>
     </div>
   );
 };
