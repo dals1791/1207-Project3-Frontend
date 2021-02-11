2a9a6794bde938cb4f05e335cca1862154ee3aeb
Sean Daly <daly.se91@gmail.com>
2021-02-10 21:31:59 -0600

    Navbar- replaced words Links with Icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// ===IMPORT REACT FONTAWESOME======
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserEdit, faUsers, faDollarSign, faHome, faFileInvoiceDollar} from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = (props) => {
   return (
@@ -9,10 +12,10 @@ const NavBar = (props) => {
       <div className="nav-bar-text">
         <ul>
           <Link to="/home" style={{ textDecoration: "none" }}>
-            <li>Home</li>
+            <li><FontAwesomeIcon  className="navbar-home-icon" style={{color: "white", fontSize: "32px"}} icon={faHome} /></li>
           </Link>
           <Link to="/transactions" style={{ textDecoration: "none" }}>
-            <li>Transactions</li>
+            <li><FontAwesomeIcon  className="navbar-transactions-icon" style={{color: "white", fontSize: "32px"}} icon={faFileInvoiceDollar} /></li>
           </Link>
 
           <li
@@ -21,14 +24,14 @@ const NavBar = (props) => {
               props.toggleAdd();
             }}
           >
-            New
+            <FontAwesomeIcon  className="navbar-add-icon" style={{color: "lawngreen", fontSize: "42px"}} icon={faDollarSign} />
           </li>
 
           <Link to="/team" style={{ textDecoration: "none" }}>
-            <li>Team</li>
+            <li><FontAwesomeIcon  className="navbar-team-icon" style={{color: "white", fontSize: "32px"}} icon={faUsers} /></li>
           </Link>
           <Link to="/userinfo" style={{ textDecoration: "none" }}>
-            <li>User</li>
+            <li><FontAwesomeIcon  className="navbar-user-icon" style={{color: "white", fontSize: "32px"}} icon={faUserEdit} /></li>
           </Link>
         </ul>
       </div>
