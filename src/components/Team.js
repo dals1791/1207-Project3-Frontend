597b22f4470e69d1f5ce0a2162fc8a511e9253d8
Sean Daly <daly.se91@gmail.com>
2021-02-12 11:32:24 -0600

    updated user bios

--- src/components/Team.js
+++ src/components/Team.js
@@ -19,8 +19,7 @@ const Team = () => {
           <p>Front-End/Back-End for user pages.</p>
           </div>
           <div className="paragraph-team1-after">
-          <p>Hobbies: Drawing, loves to hike, be out in nature, unfortunately do not have a car so cannot do so that often. 
-            .</p>
+          <p>Anny is a full stack developer from Brooklyn New York who loves to hike, but unfortunately do not own a car to do so often. For this project she handled creating the pie chart and rendering data in transactions page. Also assisted in styling for mobile, desktop and tablet.</p>
           </div>
 
       </div>
@@ -33,7 +32,7 @@ const Team = () => {
           <p>Back-End set-up, configuration and assisted with front-end pages and routes.</p>
           </div>
           <div className="paragraph-team2-after">
-          <p>Sean is a fellow student from the General Assembly Software Engineering Immersive. He has two cats.</p>
+          <p>Sean is a software engineer residing in Austin, Texas. He has two cats and loves skiing, the outdoors, and soccer. During the creation of Bugtpad, Sean worked on setting up the backend routes using Express, Mongoose, and Mongodb as well the frontend React routes and forms.</p>
           </div>
       </div>
           <div className="team-member-card3">
@@ -42,10 +41,12 @@ const Team = () => {
         <h4>Steven</h4>
         </div>
         <div className="paragraph-team3">
-          <p>blah blah blah something about this awesome team member.</p>
+          <p>Steven is born and raised in the Bronx. Steve is a full-stack developer, aspiring to specialize in front-end design. He likes to ride motorcycles during the summer and he also practices stenography, specializing in closed-captioning.
+Steve hates strawberry milk and loves chocolate milk.</p>
           </div>
           <div className="paragraph-team3-after">
-          <p>blah blah blah something about this awesome team member.</p>
+          <p>Steven is born and raised in the Bronx. Steve is a full-stack developer, aspiring to specialize in front-end design. He likes to ride motorcycles during the summer and he also practices stenography, specializing in closed-captioning.
+          Steve hates strawberry milk and loves chocolate milk.</p>
           </div>
           </div>
       </div>
