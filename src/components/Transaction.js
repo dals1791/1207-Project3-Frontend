91a9ec84589afd0fc0f5db7a6bbc9add6c7630fa
aria414 <ayng13@yahoo.com>
2021-02-21 22:11:20 -0500

    added sort by date for transactions and edites some css

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Summary from "./Summary";
-import UpdateTransaction from "./UpdateTransaction"
+import UpdateTransaction from "./UpdateTransaction";
 // ===IMPORT REACT FONTAWESOME======
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Transaction = (props) => {
   const { user, url, getUser } = props;
-  const [toggleUpdate, setToggleUpdate]= React.useState(false)
+  const [toggleUpdate, setToggleUpdate] = React.useState(false);
   // =====DESTROY ROUTE=============
   const destroyTransaction = (id) => {
     fetch(url + "/transactions/" + id, {
@@ -18,14 +18,14 @@ const Transaction = (props) => {
   };
   // ======================================
   // ============PUT ROUTE=================
-  const handleToggleUpdate= () => {
+  const handleToggleUpdate = () => {
     setToggleUpdate((toggle) => !toggle);
   };
-   
-  const[selectedTrans, setSelectedTrans]= React.useState(null)
-  const handleSetSelectedTrans =(expense)=>{
-    setSelectedTrans(expense)
-  }
+
+  const [selectedTrans, setSelectedTrans] = React.useState(null);
+  const handleSetSelectedTrans = (expense) => {
+    setSelectedTrans(expense);
+  };
   const handleUpdate = (expense) => {
     fetch(url + "/transactions/" + selectedTrans._id, {
       method: "put",
@@ -39,6 +39,10 @@ const Transaction = (props) => {
   };
   // =====================================
 
+  //States for toggling the sort for routine and non-routine transaction by date.
+  const [toggleSort, setToggleSort] = React.useState(false);
+  const [toggleSortNon, setToggleSortNon] = React.useState(false);
+
   const loaded = () => {
     /* ------------------------------------------------------
      CALCULATE THE TOTAL SPENDINGS
@@ -47,12 +51,10 @@ const Transaction = (props) => {
     const budget = user.budget; //for use as props also
     const transactions = user.transactions; // for use as props also
 
-    //- Gran only the transactions that are isExpense: true
+    //- Grab only the transactions that are isExpense: true
     const totalExpenses = user.transactions.filter((transaction) => {
       return transaction.isExpense === true;
     });
-
-    // console.log("Extracted expenses: ", totalExpenses);
 
     // Add up each expense transaction to the totalSpent
     totalExpenses.forEach((transaction) => {
@@ -67,7 +69,7 @@ const Transaction = (props) => {
       return transaction.isExpense === false;
     });
 
-    // Sum up the amount from the transaction objects
+    // Add up each deposite transaction to the depositeSum
     let depositeSum = 0;
     totalDeposite.forEach((deposite) => {
       return (depositeSum += deposite.amount);
@@ -95,97 +97,263 @@ const Transaction = (props) => {
     ];
 
     const nonRoutineExpense = user.transactions.filter((transaction) => {
-      //return transaction.isRoutine === false && transaction.isExpense === true;
       return transaction.isRoutine === false;
     });
 
-    const expenseList = nonRoutineExpense.map((expense, index) => {
-      let formatedDate = new Date(expense.time);
+    //Create an array that holds the sorted transactions Latest - Oldest
+    const sortedNonRouArr = nonRoutineExpense
+      .slice()
+      .sort((a, b) => new Date(b.time) - new Date(a.time));
 
-      let transactionType = "";
-      let spanColor = "white";
+    let togBtnNonroutine = "Oldest";
 
-      if (expense.isExpense === true) {
-        transactionType = "Expense";
-        spanColor = "#ffb0b0";
-      } else {
-        transactionType = "Deposit";
-        spanColor = "#5dca00";
-      }
+    const sort_non_routine = () => {
+      setToggleSortNon(!toggleSortNon);
+    };
 
-      return (
-        <div className="transact-card" key={index}>
-          <div>
-            <p className="transact-descr">{expense.description}</p>
-            <p className="trnact-light-font">{expense.category}</p>
+    let expenseList = [];
 
-            <p className="trnact-light-font">
-              {months[formatedDate.getMonth()]} / {formatedDate.getDate()} /
-              {formatedDate.getFullYear()} - {formatedDate.toLocaleTimeString()}
-            </p>
+    //If toggleSortNon = true, map the sorted array, else map the original array.
+    if (toggleSortNon) {
+      togBtnNonroutine = "Latest";
+      expenseList = sortedNonRouArr.map((expense, index) => {
+        //Need to use New Date() to get the transaction date into a proper format.
+        // When it was created from date.now() it is actaully an integer of how many milliseconds...
+        let formatedDate = new Date(expense.time);
+
+        // Set the color for the transaction $$.
+        // If expense, make it pink. If deposite, make it green
+        let spanColor = "white";
+
+        if (expense.isExpense === true) {
+          spanColor = "#ffb0b0";
+        } else {
+          spanColor = "#5dca00";
+        }
+
+        return (
+          <div className="transact-card" key={index}>
+            <div>
+              <p className="transact-descr">{expense.description}</p>
+              <p className="trnact-light-font">{expense.category}</p>
+
+              <p className="trnact-light-font">
+                {months[formatedDate.getMonth()]} / {formatedDate.getDate()} /
+                {formatedDate.getFullYear()} -{" "}
+                {formatedDate.toLocaleTimeString()}
+              </p>
+            </div>
+
+            <span style={{ color: spanColor }}>${expense.amount}</span>
+            <div
+              className="destroy-button"
+              onClick={() => {
+                destroyTransaction(expense._id);
+              }}
+            >
+              <FontAwesomeIcon
+                className="navbar-home-icon"
+                style={{ color: "white", fontSize: "1.5rem" }}
+                icon={faTrashAlt}
+              />
+            </div>
+            <div
+              onClick={() => {
+                handleToggleUpdate();
+                handleSetSelectedTrans(expense);
+              }}
+            >
+              <FontAwesomeIcon
+                className="navbar-home-icon"
+                style={{ color: "white", fontSize: "1.5rem" }}
+                icon={faEdit}
+              />
+            </div>
           </div>
+        );
+      });
+    } else {
+      expenseList = nonRoutineExpense.map((expense, index) => {
+        //Need to use New Date() to get the transaction date into a proper format.
+        // When it was created from date.now() it is actaully an integer of how many milliseconds...
+        let formatedDate = new Date(expense.time);
 
-          <span style={{ color: spanColor }}>${expense.amount}</span>
-          <div
-            className="destroy-button"
-            onClick={() => {
-              destroyTransaction(expense._id);
-            }}
-          >
-            <FontAwesomeIcon  className="navbar-home-icon" style={{color: "white", fontSize: "1.5rem"}} icon={faTrashAlt} />
+        // Set the color for the transaction $$.
+        // If expense, make it pink. If deposite, make it green
+        let spanColor = "white";
+
+        if (expense.isExpense === true) {
+          spanColor = "#ffb0b0";
+        } else {
+          spanColor = "#5dca00";
+        }
+
+        return (
+          <div className="transact-card" key={index}>
+            <div>
+              <p className="transact-descr">{expense.description}</p>
+              <p className="trnact-light-font">{expense.category}</p>
+
+              <p className="trnact-light-font">
+                {months[formatedDate.getMonth()]} / {formatedDate.getDate()} /
+                {formatedDate.getFullYear()} -{" "}
+                {formatedDate.toLocaleTimeString()}
+              </p>
+            </div>
+
+            <span style={{ color: spanColor }}>${expense.amount}</span>
+            <div
+              className="destroy-button"
+              onClick={() => {
+                destroyTransaction(expense._id);
+              }}
+            >
+              <FontAwesomeIcon
+                className="navbar-home-icon"
+                style={{ color: "white", fontSize: "1.5rem" }}
+                icon={faTrashAlt}
+              />
+            </div>
+            <div
+              onClick={() => {
+                handleToggleUpdate();
+                handleSetSelectedTrans(expense);
+              }}
+            >
+              <FontAwesomeIcon
+                className="navbar-home-icon"
+                style={{ color: "white", fontSize: "1.5rem" }}
+                icon={faEdit}
+              />
+            </div>
           </div>
-          <div onClick={()=>{handleToggleUpdate(); handleSetSelectedTrans(expense);}}><FontAwesomeIcon  className="navbar-home-icon" style={{color: "white", fontSize: "1.5rem"}} icon={faEdit} /></div>
-        </div>
-      );
-    });
+        );
+      });
+    }
 
-    console.log("User info in transactions: ", user.transactions);
-    console.log("All non-routine expenses: ", nonRoutineExpense);
+    // console.log("User info in transactions: ", user.transactions);
+    // console.log("All non-routine expenses: ", nonRoutineExpense);
 
     /* ------------------------------------------------------
-    GET TRANSACTIONS - BILLS - IS AN EXPENSE
+    GET TRANSACTIONS - BILLS 
   ------------------------------------------------------ */
     const routineExpense = user.transactions.filter((transaction) => {
       return transaction.isRoutine === true;
     });
 
-    const routineList = routineExpense.map((expense, index) => {
-      let formatedDate = new Date(expense.time);
-      let transactionType = "";
-      let spanColor = "white";
+    const sortedRoutine = routineExpense
+      .slice()
+      .sort((a, b) => new Date(b.time) - new Date(a.time));
 
-      if (expense.isExpense === true) {
-        transactionType = "Expense";
-        spanColor = "#ffb0b0";
-      } else {
-        transactionType = "Deposit";
-        spanColor = "#5dca00";
-      }
-      return (
-        <div className="transact-card" key={index}>
-          <div>
-            <p className="transact-descr">{expense.description}</p>
-            <p className="trnact-light-font">{expense.category}</p>
-            <p className="trnact-light-font">
-              {months[formatedDate.getMonth()]} / {formatedDate.getDate()} /
-              {formatedDate.getFullYear()} - {formatedDate.toLocaleTimeString()}
-            </p>
-          </div>
+    const sort_routine = () => {
+      setToggleSort(!toggleSort);
+    };
 
-          <span style={{ color: spanColor }}>${expense.amount}</span>
-          <div
-            className="destroy-button"
-            onClick={() => {
-              destroyTransaction(expense._id);
-            }}
-          >
-            <FontAwesomeIcon  className="navbar-home-icon" style={{color: "white", fontSize: "1.5rem"}} icon={faTrashAlt} />
+    let togBtnRoutine = "Oldest";
+    let routineList = [];
+
+    if (toggleSort) {
+      togBtnRoutine = "Latest";
+      routineList = sortedRoutine.map((expense, index) => {
+        let formatedDate = new Date(expense.time);
+        let spanColor = "white";
+
+        if (expense.isExpense === true) {
+          spanColor = "#ffb0b0";
+        } else {
+          spanColor = "#5dca00";
+        }
+        return (
+          <div className="transact-card" key={index}>
+            <div>
+              <p className="transact-descr">{expense.description}</p>
+              <p className="trnact-light-font">{expense.category}</p>
+              <p className="trnact-light-font">
+                {months[formatedDate.getMonth()]} / {formatedDate.getDate()} /
+                {formatedDate.getFullYear()} -{" "}
+                {formatedDate.toLocaleTimeString()}
+              </p>
+            </div>
+
+            <span style={{ color: spanColor }}>${expense.amount}</span>
+            <div
+              className="destroy-button"
+              onClick={() => {
+                destroyTransaction(expense._id);
+              }}
+            >
+              <FontAwesomeIcon
+                className="navbar-home-icon"
+                style={{ color: "white", fontSize: "1.5rem" }}
+                icon={faTrashAlt}
+              />
+            </div>
+            <div
+              onClick={() => {
+                handleToggleUpdate();
+                handleSetSelectedTrans(expense);
+              }}
+            >
+              <FontAwesomeIcon
+                className="navbar-home-icon"
+                style={{ color: "white", fontSize: "1.5rem" }}
+                icon={faEdit}
+              />
+            </div>
           </div>
-          <div onClick={()=>{handleToggleUpdate(); handleSetSelectedTrans(expense);}}><FontAwesomeIcon  className="navbar-home-icon" style={{color: "white", fontSize: "1.5rem"}} icon={faEdit} /></div>
-        </div>
-      );
-    });
-    console.log("All routine expenses: ", routineExpense);
+        );
+      });
+    } else {
+      routineList = routineExpense.map((expense, index) => {
+        let formatedDate = new Date(expense.time);
+        let spanColor = "white";
+
+        if (expense.isExpense === true) {
+          spanColor = "#ffb0b0";
+        } else {
+          spanColor = "#5dca00";
+        }
+        return (
+          <div className="transact-card" key={index}>
+            <div>
+              <p className="transact-descr">{expense.description}</p>
+              <p className="trnact-light-font">{expense.category}</p>
+              <p className="trnact-light-font">
+                {months[formatedDate.getMonth()]} / {formatedDate.getDate()} /
+                {formatedDate.getFullYear()} -{" "}
+                {formatedDate.toLocaleTimeString()}
+              </p>
+            </div>
+
+            <span style={{ color: spanColor }}>${expense.amount}</span>
+            <div
+              className="destroy-button"
+              onClick={() => {
+                destroyTransaction(expense._id);
+              }}
+            >
+              <FontAwesomeIcon
+                className="navbar-home-icon"
+                style={{ color: "white", fontSize: "1.5rem" }}
+                icon={faTrashAlt}
+              />
+            </div>
+            <div
+              onClick={() => {
+                handleToggleUpdate();
+                handleSetSelectedTrans(expense);
+              }}
+            >
+              <FontAwesomeIcon
+                className="navbar-home-icon"
+                style={{ color: "white", fontSize: "1.5rem" }}
+                icon={faEdit}
+              />
+            </div>
+          </div>
+        );
+      });
+    }
 
     return (
       <div className="transaction-component">
@@ -201,12 +369,14 @@ const Transaction = (props) => {
 
         <div className="all-transactions">
           <section>
-            <h2>Latest</h2>
+            <h2>Non Routine</h2>
+            <button onClick={sort_non_routine}>{togBtnNonroutine}</button>
             {expenseList}
           </section>
 
           <section>
-            <h2>Bills</h2>
+            <h2>Routine Bills</h2>
+            <button onClick={sort_routine}>{togBtnRoutine}</button>
             {routineList}
           </section>
         </div>
@@ -218,19 +388,20 @@ const Transaction = (props) => {
     return <h1>Page loading...</h1>;
   };
 
-  return (<>
-    {user ? loaded() : loading()}
-    <div className="add-transaction-container">
-    {toggleUpdate ? (
-      <UpdateTransaction
-      transaction={selectedTrans}
-        handleSubmit={handleUpdate}
-        toggleAdd={handleToggleUpdate}
-        setToggleUpdate={setToggleUpdate}
-      />
-    ) : null}
-    </div>
+  return (
+    <>
+      {user ? loaded() : loading()}
+      <div className="add-transaction-container">
+        {toggleUpdate ? (
+          <UpdateTransaction
+            transaction={selectedTrans}
+            handleSubmit={handleUpdate}
+            toggleAdd={handleToggleUpdate}
+            setToggleUpdate={setToggleUpdate}
+          />
+        ) : null}
+      </div>
     </>
-    );
+  );
 };
 export default Transaction;
